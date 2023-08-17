196f8d8c8f21f506f1c9ae6635290f59764150ee
abrarzahed <abrarzahed6986@gmail.com>
2023-08-18 03:01:04 +0600

    modal popup

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+// imports
 import "./styles/style.scss";
+
+// selectors
+const menuItems = document.querySelectorAll(".navbar-menu__items-item");
+const overlay = document.querySelector(".overlay");
+const dropdown = document.querySelector(".dropdown");
+const hero = document.querySelector(".hero");
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    if (
+      e.target.classList.contains("search-icon") ||
+      e.target.classList.contains("fa-magnifying-glass")
+    )
+      return;
+    dropdown.classList.remove("hidden");
+  });
+});
+
+hero.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("hero")) return;
+  dropdown.classList.add("hidden");
+});
