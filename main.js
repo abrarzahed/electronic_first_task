11f1a261f08b5081eb8a1f7aa046a1a25aff372f
abrarzahed <abrarzahed6986@gmail.com>
2023-08-18 12:04:11 +0600

    close dropdown

--- main.js
+++ main.js
@@ -3,22 +3,40 @@ import "./styles/style.scss";
 
 // selectors
 const menuItems = document.querySelectorAll(".navbar-menu__items-item");
-const overlay = document.querySelector(".overlay");
 const dropdown = document.querySelector(".dropdown");
-const hero = document.querySelector(".hero");
+const btnClose = document.querySelector(".dropdown-close-btn");
+// const hero = document.querySelector(".hero");
+
+const openDropdown = () => {
+  dropdown.classList.remove("hidden");
+};
+
+const closeDropdown = () => {
+  dropdown.classList.add("hidden");
+};
 
 menuItems.forEach((item) => {
   item.addEventListener("click", (e) => {
     if (
       e.target.classList.contains("search-icon") ||
       e.target.classList.contains("fa-magnifying-glass")
-    )
+    ) {
       return;
-    dropdown.classList.remove("hidden");
+    }
+    openDropdown();
   });
 });
 
+/*
 hero.addEventListener("click", (e) => {
   if (!e.target.classList.contains("hero")) return;
-  dropdown.classList.add("hidden");
+  closeDropdown();
 });
+*/
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    closeDropdown();
+  }
+});
+btnClose.addEventListener("click", closeDropdown);
